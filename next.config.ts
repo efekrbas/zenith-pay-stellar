5eeff8a4e2f5a5baceeda2a6d27ab501a84701b8
Efe Kırbaş <efekrbass@gmail.com>
2026-03-22 06:12:18 +0300

    fix: resolve Sentry configuration type errors in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,28 +13,4 @@ export default withSentryConfig(nextConfig, {
   silent: true,
   org: "zenith-pay",
   project: "zenith-pay-stellar",
-}, {
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source_maps for better stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Transpiles SDK to be compatible with IE11 (increases bundle size)
-  transpileClientSDK: true,
-
-  // Routes HTTP requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-  tunnelRoute: "/monitoring",
-
-  // Hides source maps from control panel (ref: https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/tree-shaking/)
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors.
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
 });
